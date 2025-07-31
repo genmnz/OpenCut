fdc0a58607ba5ea2fee5099ddf1a3a689ee27448
melad <meladnaieem@yahoo.com>
2025-08-01 01:29:25 +0300

    Should not black out the entier element to make it recognizalble

--- apps/web/src/components/editor/timeline/timeline-element.tsx
+++ apps/web/src/components/editor/timeline/timeline-element.tsx
@@ -360,7 +360,7 @@ export function TimelineElement({
             </div>
 
             {element.hidden && (
-              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center pointer-events-none">
+              <div className="absolute inset-0 bg-black opacity-65 flex items-center justify-center pointer-events-none">
                 {isAudio ? (
                   <VolumeX className="h-6 w-6 text-white" />
                 ) : (
